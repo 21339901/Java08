600a5f7d105a428704b7cccb5625126cd009e8b5
21339901 <113246766+21339901@users.noreply.github.com>
2022-09-12 22:04:22 +0800

    Create Class07.java

---
+++ Class07.java
@@ -0,0 +1,25 @@
+package Java08;
+
+public class Class07 {
+	public static void main(String[] args) {
+		
+		Ctest evenOrOdd = new Ctest();
+			evenOrOdd.x = 3;
+			evenOrOdd.test();
+			evenOrOdd.x = 8;
+			evenOrOdd.test();
+			evenOrOdd.x = 0;
+			evenOrOdd.test();
+			}
+}
+class Ctest{
+	int x;
+	void test() {
+		if (x == 0) 
+			System.out.println("此數為0");
+		else if (x % 2 == 0) 
+			System.out.println("此數為偶數");
+		else if (x % 2 == 1) 
+			System.out.println("此數為奇數");
+	}
+}
